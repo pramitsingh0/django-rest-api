7bfc594f2aeafe2c34b5682407739f50eb6f1ab9
pramitsingh0 <pramitsingh0@gmail.com>
2024-01-21 20:28:10 +0530

    added rest framework and model serializers

--- backend/products/models.py
+++ backend/products/models.py
@@ -1,9 +1,14 @@
-from django.db import models
+from django.db import models  # type: ignore
 
 
 # Create your models here.
-#
+
+
 class Product(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=15, decimal_places=2, default=99.99)
+
+    @property
+    def sale_price(self):
+        return "%.2f" % (float(float(self.price) * 0.8))  # type: ignore
